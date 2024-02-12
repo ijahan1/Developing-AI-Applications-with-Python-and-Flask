988ba7c9dad3c68dcc22d9e517ea5174b1a9a284
Israt Jahan <98222327+ijahan1@users.noreply.github.com>
2024-02-12 13:31:56 -0500

    Files Added
    Instructions:
    Open a new terminal and type and execute:
    >>>python3
    If the above command runs successfully, type and execute the command below:
    >>>import mymath
    If the command above runs successfully, type and execute the following command:
    >>>mymath.basic.add(2,5)
    Follow the same approach for all module

---
+++ packaging/mymath/geometry.py
@@ -0,0 +1,13 @@
+def area_of_rectangle(h,w):
+    """
+    This function returns the area of a rectangle of height h and width w
+    """
+    return h * w
+
+def area_of_circle(r):
+    """
+    This function returns the area of a circle with radius r
+    """
+    return 3.14 * (r **2)
+
+
