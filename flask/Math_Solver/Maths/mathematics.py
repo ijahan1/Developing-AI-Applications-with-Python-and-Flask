d2296ae17d3e10a1ab4d82597762c8e2d594ad60
Israt Jahan <98222327+ijahan1@users.noreply.github.com>
2024-02-13 01:01:07 -0500

    Files Added
    Instructions:
    --------------
    $cd /home/project/Math_Solver && python3.11 server.py

---
+++ flask/Math_Solver/Maths/mathematics.py
@@ -0,0 +1,14 @@
+"""
+This function takes two numbers as input and return the sum of input numbers
+"""
+def summation(a,b):
+    result = a + b
+    return result
+
+def subtraction(a,b):
+    result = a - b
+    return result
+
+def multiplication(a,b):
+    result = a * b
+    return result
